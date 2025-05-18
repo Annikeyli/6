0d368db10c6d0547af46186596d64744f39ea228
Annikeyli <anna20061010@yandex.ru>
2025-05-18 19:07:15 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,14 @@
-cout << "\n Spisok \n";
-MyList<Znak> list;
-for (Znak x : gr)list.addEnd(x);
-cout << list<< "\n";
+#include <iostream>
+using namespace std;
+#include <string.h> 
+#include "list.h" 
+#include <string> 
+
+
+int main()
+{
+    cout << "\n Spisok \n";
+    MyList<Znak> list;
+    for (Znak x: gr)list.addEnd(x);
+    cout << list << "\n";
+}
