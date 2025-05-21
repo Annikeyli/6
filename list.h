5da08fb1ec08410f4c0b97aeaeb92592f1f266a2
Annikeyli <anna20061010@yandex.ru>
2025-05-21 14:12:57 +0300

    Update list.h

--- list.h
+++ list.h
@@ -2,6 +2,7 @@
 using namespace std;
 #include <string.h> 
 #include "Znak.h"
+
 template <class T>
 class MyList {
     class Node {
@@ -11,88 +12,117 @@ class MyList {
         Node(T info, Node* next) :info(info), next(next) {};
         Node(T info) :info(info), next(nullptr) {};
         Node() {};
-
     };
     Node* head = nullptr;
     Node* tail = nullptr;
 public:
     void addBegin(T a);
     void addEnd(T a);
-    void deleteList();
     Node* listCopy(Node* head, Node*& tailCopy);
     int insertList(Node* head, int x);
-    bool digitIntNode(Node* head, T a);
     bool IsEmpty();
     void deleteEl(T a);
+    void deleteList();
     friend ostream& operator<<(ostream& os, MyList<T>& l) {
-        for (Node* t = l.head; t != nullptr; t = t->next)os << t->info << " \n";
+        for (Node* t = l.head; t != nullptr; t = t->next) os << t->info << " \n";
         return os;
     }
-    /*bool operator==(Node* head, T a) {
-        Node* h
-            return h->info == a;
-    }*/
-
 };
 
-
 template <class T>
-inline void MyList<T>::addBegin(T a) { // поставили &, чтобы можно было что-то присвоить и это появится в мэйне 
-    Node* t = new Node(a, head); // создали новый узел 
+void MyList<T>::addBegin(T a) {
+    Node* t = new Node(a, head);
     if (!head) head = tail = t;
     else head = t;
-
 }
 
 template <class T>
 void MyList<T>::addEnd(T a) {
-    Node* t = new Node(a); // создали новый узел 
+    Node* t = new Node(a);
     if (!head) {
         head = tail = t; return;
     }
     tail->next = t;
     tail = t;
 }
+
 template <class T>
 void MyList<T>::deleteList() {
-    if (head != nullptr) {
-        Node* t = head;
-        while (t) {
-            Node* g = t;
-            t = t->next;
-            t = nullptr;
-            delete g;
-        }
+    Node* t = head;
+    while (t) {
+        Node* g = t;
+        t = t->next;
+        delete g;
     }
-    head = nullptr;
+    head = tail = nullptr;
 }
 
 template<class T>
-inline bool MyList<T>::digitIntNode(Node* head, T x)
-{
-    Node* h;
-    for (h = head; h && h->info == x; h = h->next);
-    if (h)return true;
-    return false;
-}
-
-template<class T>
-inline bool MyList<T>::IsEmpty()
-{
+bool MyList<T>::IsEmpty() {
     return head == nullptr;
+
 }
 
 template<class T>
-inline void MyList<T>::deleteEl(T a)
-{
-    if (head != nullptr) {
-        Node* t = head;
-        while (t) {
-            Node* g = t;
-            t = t->next;
-            if (t == a){
-                delete g;
+void MyList<T>::deleteEl(T a) {
+    if (head == nullptr) return;
+
+    Node* prev = nullptr;
+    Node* curr = head;
+
+    while (curr != nullptr) {
+        if (curr->info == a) {
+            if (prev == nullptr) {
+                head = curr->next;
+                if (tail == curr) tail = nullptr;
             }
+            else {
+                prev->next = curr->next;
+                if (tail == curr) tail = prev;
+            }
+            Node* toDelete = curr;
+            curr = curr->next;
+            delete toDelete;
+        }
+        else {
+            prev = curr;
+            curr = curr->next;
         }
     }
+}
+
+template <class T>
+typename MyList<T>::Node* MyList<T>::listCopy(Node* head, Node*& tailCopy) {
+    Node* head1 = nullptr;
+    Node* prev = nullptr;
+    tailCopy = nullptr;
+
+    for (Node* h = head; h; h = h->next) {
+        Node* newNode = new Node(h->info);
+        if (!head1) {
+            head1 = prev = newNode;
+        }
+        else {
+            prev->next = newNode;
+            prev = newNode;
+        }
+        tailCopy = newNode;
+    }
+    return head1;
+}
+
+template <class T>
+int MyList<T>::insertList(Node* head, int x) {
+    Node* nodeX = nullptr;
+    Node* h;
+    for (h = head; h && h->info.getNumber() != x; h = h->next);
+
+    if (h) {
+        Node* tailCopy = nullptr;
+        Node* headCopy = listCopy(head, tailCopy);
+        tailCopy->next = h->next;
+        h->next = headCopy;
+        return 0;
+    }
+    return -1;
 }
