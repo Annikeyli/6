931739a2dd13807e105558cde22fab5743ef308e
Annikeyli <anna20061010@yandex.ru>
2025-05-21 14:13:20 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,14 +1,31 @@
 #include <iostream>
 using namespace std;
 #include <string.h> 
-#include "list.h" 
+#include "MyList.h" 
 #include <string> 
-
 
 int main()
 {
-    cout << "\n Spisok \n";
     MyList<Znak> list;
-    for (Znak x: gr)list.addEnd(x);
+    cout << "\nSpisok\n";
+    
+    Znak z1("Ivanov", "Ivan", Zodiak::Aries, 10, 3, 1990);
+    Znak z2("Petrov", "Petr", Zodiak::Taurus, 15, 4, 1985);
+    Znak z3("Petrova", "Lena", Zodiak::Libra, 10, 10, 2010);
+    list.addEnd(z1);
+    list.addEnd(z2);
+    list.addBegin(z3);
+
     cout << list << "\n";
+
+    cout << "Is empty? " << (list.IsEmpty() ? "yes" : "no") << "\n";
+       
+    list.deleteEl(z2);
+    cout << "\nAfter delete element\n" << list << "\n";
+
+    list.deleteList();
+    cout << "After delete list\n";
+    cout << "Is empty? " << (list.IsEmpty() ? "yes" : "no") << "\n";
+
+    return 0;
 }
